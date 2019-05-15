b42a38b1e81252b26ba963d797bddea0346913e4
user129 <33879349+user129@users.noreply.github.com>
2019-05-16 00:14:26 +0300

    Add files via upload
    user student teacher

---
+++ src/Teacher.java
@@ -0,0 +1,20 @@
+public  class Teacher implements User {
+
+    public void register(String username,String password,String name,String surname,String userid,String email) {
+
+    }
+
+
+    public void login(String username,String password) {
+
+    }
+
+    public void logout() {
+
+    }
+
+    Courses[] courses;
+    public void addCourse(){}
+    public void remove_Course(){}
+
+}
---
+++ src/User.java
@@ -0,0 +1,6 @@
+public interface User {
+
+    public  void register(String username,String password,String name,String surname,String userid,String email);
+    public  void login(String username,String password);
+    public  void logout();
+}
