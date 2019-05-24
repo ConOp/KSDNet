670206d04a5cc68360aeb2d9d1b31a54b52378f2
StathisArmaganidis <stathisarmaganidis@hotmail.com>
2019-05-24 16:17:53 +0300

    Revert "Revert "The real fix""
    This reverts commit 3359529addd45a8b4097c27d48e819edecc693de.

--- KSDNetWeb/src/Servlets/CourseServlet.java
+++ KSDNetWeb/src/Servlets/CourseServlet.java
@@ -1,3 +1,5 @@
+package Servlets;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -5,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "CourseServlet")
+@WebServlet(name = "Servlets.CourseServlet")
 public class CourseServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
