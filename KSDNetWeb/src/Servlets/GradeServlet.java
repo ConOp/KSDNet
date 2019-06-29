67c7567238e0204478e59ba602cb6ac72d5cf16d
StathisArmaganidis <stathisarmaganidis@hotmail.com>
2019-06-29 18:52:00 +0300

    Grades#2

--- KSDNetWeb/src/Servlets/GradeServlet.java
+++ KSDNetWeb/src/Servlets/GradeServlet.java
@@ -12,6 +12,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebServlet(name = "GradeServlet", value="/GradingTeacher")
 public class GradeServlet extends HttpServlet {
@@ -26,59 +27,65 @@ public class GradeServlet extends HttpServlet {
         }
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html; charset=UTF-8"); //θέτει τον τύπο περιεχομένου της απάντησης που αποστέλλεται στον πελάτη, εάν η απάντηση δεν έχει δεσμευτεί ακόμα
         request.setCharacterEncoding("UTF-8"); //κωδικοποίηση χαρακτήρων request
         response.setCharacterEncoding("UTF-8");
 
+        PrintWriter  out=response.getWriter();
         String testname = "C57832";//(String)request.getParameter();
         ResultSet Rs=null;
+
+
+        out.println("<!DOCTYPE HTML>" +
+                "<html>" +
+                "<head>" +
+                "<meta charset=\"UTF-8\">" +
+                "<title>Teacher Homepage</title>" +
+                "<link href=\"./bootstrap/css/bootstrap.css\" rel=\"stylesheet\">" +
+                "<link href=\"./bootstrap/css/bootstrap-grid.css\" rel=\"stylesheet\">" +
+                "<link href=\"./bootstrap/css/bootstrap-reboot.css\" rel=\"stylesheet\"><link rel=\"stylesheet\" href=\"./bootstrap/css/bootstrap.css\">" +
+                "</head><body><div class=\"container d-flex justify-content-center\">\n" +
+                "<div class=\"shadow p-3 mb-5 bg-white rounded\">\n" +
+                "<div class=\"card text-center \" style=\"width: 45rem;\"><div class=\"card-body\">\n" +
+                "<h5 class=\"card-title\">Κατάλογος Ομάδων</h5>\n" +
+                "<div class = \"col\">\n" +
+                "<form method=\"post\" action=\"/TeacherHomepage\"><ul class=\"list-group list-group-flush\">");
+
+
         try{
             Connection con = ds.getConnection();
-            PreparedStatement st = con.prepareStatement("SELECT  group_id,totalgrade FROM groups WHERE course_id='"+testname+"'"); //παίρνουμε το userid από τη βάση
+            PreparedStatement st = con.prepareStatement("SELECT  group_id,totalgrade FROM groups WHERE course_id='"+testname+"' and  totalgrade=0 asc"); //παίρνουμε το userid από τη βάση
             Rs = st.executeQuery();
-        }catch(Exception e){}
-
-
-        PrintWriter out=response.getWriter();
-        out.println("    <!DOCTYPE html>\n" +
-                "    <html>\n" +
-                "    <head>\n" +
-                "     <title>Table with database</title>\n" +
-                "     <style>\n" +
-                "      table {\n" +
-                "       border-collapse: collapse;\n" +
-                "       width: 100%;\n" +
-                "       color: #588c7e;\n" +
-                "       font-family: monospace;\n" +
-                "       font-size: 25px;\n" +
-                "       text-align: left;\n" +
-                "         } \n" +
-                "      th {\n" +
-                "       background-color: #588c7e;\n" +
-                "       color: white;\n" +
-                "        }\n" +
-                "      tr:nth-child(even) {background-color: #f2f2f2}\n" +
-                "     </style>\n" +
-                "    </head>\n" +
-                "    <body>\n" +
-                "     <table>\n" +
-                "     <tr>\n" +
-                "      <th>Group ID</th> \n" +
-                "      <th>Grade</th> \n" +
-                "     </tr>\n");
-        try{
-            while(Rs.next()) {
-                out.println("<tr><td>"+Rs.getString("group_id")+"</td><td>"+Rs.getString("totalgrade")+"</td><td>\n");
-                System.out.println(Rs.getString(("group_id")+" | "+ Rs.getString("totalgrade")));
+            PrintResults(Rs,out);
+            if (request.getParameter("logout") != null) {
+                request.getSession().removeAttribute("username");
+                request.getSession().invalidate();
+                response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+                response.sendRedirect("/TeacherHomepage");
             }
-        }catch(Exception e){
-            System.out.println("Shit");
-        }
+            st.close();
 
-        out.println("</table>\n" +
-                "    </body>\n" +
-                "    </html>");
+        }catch(Exception e){
+
+        }
+    }
+    protected void PrintResults(ResultSet rs,PrintWriter out) {
+        try {
+            while (rs.next()){
+                out.println("<a href=\"#\" class=\"list-group-item list-group-item-action\">"+rs.getString("group_id")+"</a>");
+            }
+            out.println("</ul><br>" +
+                    "<input type=\"button\" onclick=\"location.href='new_course.html'\" value=\"Add new course\"><br>" +
+                    "<br><input type=\"submit\" id=\"log\" value=\"LOGOUT\" name=\"logout\">\n" +
+                    "</form></div></div></div></div></div>" +
+                    "<script src=\"./bootstrap/js/bootstrap.bundle.js\" ></script>" +
+                    "<script src=\"./bootstrap/js/bootstrap.js\" ></script>" +
+                    "</body>" +
+                    "</html>");
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
 
     }
 }
